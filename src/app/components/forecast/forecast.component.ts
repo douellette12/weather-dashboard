ce3ffa90d10cf8358c9ad52f9d7144b2a3926e77
David Ouellette <david.ouellette12@protonmail.com>
2024-05-25 07:49:34 -0400

    feat(forecast): logic for on change weather data

--- src/app/components/forecast/forecast.component.ts
+++ src/app/components/forecast/forecast.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit, output } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, output } from '@angular/core';
 
 @Component({
   selector: 'app-forecast',
@@ -8,10 +8,10 @@ import { Component, Input, OnInit, output } from '@angular/core';
   templateUrl: './forecast.component.html',
   styleUrl: './forecast.component.scss'
 })
-export class ForecastComponent implements OnInit {
+export class ForecastComponent implements OnChanges {
 
-  date: Date = new Date()
-
+  date?: Date
+  
   datesArray = Array.from({ length: 4 }, (_, i) => {
     const date = new Date();
     date.setDate(date.getDate() + i + 1);
@@ -21,10 +21,31 @@ export class ForecastComponent implements OnInit {
   @Input()
   forecastData?: any
 
-  ngOnInit(): void {
-    setInterval(() => {
-          this.date = new Date();
-        }, 1000);
+  futureForecast: any[] = []
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.futureForecast = []
+    if(this.forecastData){
+      
+      this.datesArray = Array.from({ length: 4 }, (_, i) => {
+        const date = new Date(this.forecastData.list[0].dt*1000);
+        date.setDate(date.getDate() + i + 1);
+        return date;
+      });
+
+      this.date = new Date(this.forecastData.list[0].dt*1000)
+      let tomorrow = new Date(this.date)
+      tomorrow.setDate(tomorrow.getDate() + 1)
+      
+      for (let i = 0; i < this.forecastData.list.length; i++){
+        this.datesArray.forEach((date) => {
+          if (date.toString() == new Date(this.forecastData.list[i].dt*1000).toString()){
+            this.futureForecast.push(this.forecastData.list[i])
+          }
+        })
+      }
     }
+  }
+
 
 }
